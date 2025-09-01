bf57499e4c4a40677af98577f562bd0f45a3176e
EnzoPandaBRYT <enzocargame@gmail.com>
2025-09-01 12:08:49 -0300

    Menu de Pausa adicionado

--- gui/scripts/pause_menu.gd
+++ gui/scripts/pause_menu.gd
@@ -1,7 +1,24 @@
 extends Control
 
+func _ready() -> void:
+	$AnimationPlayer.play("RESET")
+
 func resume():
 	get_tree().paused = false
+	$AnimationPlayer.play_backwards("blur")
+	if $AnimationPlayer.animation_finished:
+		$".".visible = false
 	
-func resume():
-	get_tree().paused = false
+func pause():
+	$".".visible = true
+	get_tree().paused = true
+	$AnimationPlayer.play("blur")
+	
+func getEsc():
+	if Input.is_action_just_pressed("pause_game") and !get_tree().paused:
+		pause()
+	elif Input.is_action_just_pressed("pause_game") and get_tree().paused:
+		resume()
+		
+func _process(delta: float) -> void:
+	getEsc()
