1411b44f95e6ca808ee3937ea4481253a96bc117
EnzoPandaBRYT <enzocargame@gmail.com>
2025-09-01 12:15:36 -0300

    Adicionado Menu de Pause

--- gui/scripts/pause_menu.gd
+++ gui/scripts/pause_menu.gd
@@ -22,3 +22,12 @@ func getEsc():
 		
 func _process(delta: float) -> void:
 	getEsc()
+
+func _on_back_pressed() -> void:
+	resume()
+
+func _on_restart_pressed() -> void:
+	get_tree().reload_current_scene()
+	
+func _on_exit_pressed() -> void:
+	get_tree().change_scene_to_file("res://gui/scenes/main_menu.tscn")
